019b43b7966e088b2e95fe1fd8ab1c5914018575
KumazakiRyoha <1593713783@qq.com>
2023-11-04 22:47:23 +0900

    commit 2

--- BTree.go
+++ BTree.go
@@ -33,6 +33,13 @@ func (node BNode) setHeader(btype uint16, nkeys uint16) {
 	binary.LittleEndian.PutUint16(node.data[2:4], nkeys)
 }
 
+/*
+*
+|------|------|---------------|------------|------------------|
+| 类型 | 键数 | 指针列表       | 偏移量列表  | 键值对数据        	  |
+|------|------|---------------|------------|------------------|
+|  2字节 |  2字节 | 键数 * 8字节    | 键数 * 2字节 | ...           |
+*/
 func (node BNode) getPtr(idx uint16) uint16 {
 	assert(idx < node.nkeys())
 	pos := HEADER + 8*idx
@@ -43,6 +50,44 @@ func (node BNode) setPtr(idx uint16, val uint64) {
 	assert(idx < node.nkeys())
 	pos := HEADER + 8*idx
 	binary.LittleEndian.PutUint64(node.data[pos:], val)
+}
+
+func offsetPos(node BNode, idx uint16) uint16 {
+	assert(1 <= idx && idx <= node.nkeys())
+	// 第一个键值对的偏移量不存储，所以计算第idx个的偏移量是需要减1
+	return HEADER + 8*node.nkeys() + 2*(idx-1)
+}
+
+func (node BNode) getOffset(idx uint16) uint16 {
+	if idx == 0 {
+		return 0
+	}
+	return binary.LittleEndian.Uint16(node.data[offsetPos(node, idx):])
+}
+
+func (node BNode) setOffset(idx uint16, offset uint16) {
+	binary.LittleEndian.PutUint16(node.data[offsetPos(node, idx):], offset)
+}
+
+// key-values
+func (node BNode) kvPos(idx uint16) uint16 {
+	assert(idx <= node.nkeys())
+	return HEADER + 8*node.nkeys() + 2*node.nkeys() + node.getOffset(idx)
+}
+
+func (node BNode) getKey(idx uint16) []byte {
+	assert(idx < node.nkeys())
+	pos := node.kvPos(idx)
+	klen := binary.LittleEndian.Uint16(node.data[pos:])
+	return node.data[pos+4:][:klen]
+}
+
+func (node BNode) getVal(idx uint16) []byte {
+	assert(idx < node.nkeys())
+	pos := node.kvPos(idx)
+	klen := binary.LittleEndian.Uint16(node.data[pos+0:])
+	vlen := binary.LittleEndian.Uint16(node.data[pos+2:])
+	return node.data[pos+4+klen:][:vlen]
 }
 
 type BTree struct {
